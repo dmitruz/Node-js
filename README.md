defe49fd4b806eae27160c40ee6bbd8cf74121f1
dmitruz <dmitruz970@gmail.com>
2022-05-19 20:45:00 +0300

    update hw

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goit-node-hw-01
+
 Node.js hw 1
+
+!!!! Link for pictures !!!! https://monosnap.com/list/628680f394280558562364e2
--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 //npm start run dev  for launching project
 
-console.log(BHBHGBU );
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+(async () => {
+  const argv = program.opts();
+
+  async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case 'list': {
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
+
+      case 'get': {
+        const data = await getContactById(id);
+        console.table(data);
+        break;
+      }
+
+      case 'add': {
+        await addContact(name, email, phone);
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
+
+      case 'remove': {
+        await removeContact(id);
+        const data = await listContacts();
+        console.table(data);
+        break;
+      }
+
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  }
+
+  await invokeAction(argv);
+})();
